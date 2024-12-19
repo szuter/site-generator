81fe3854c35a72a13fea49b6eed23b8e8a7debf2
szuter <piotrstrzelec1989@gmail.com>
2024-12-19 10:05:34 +0100

    added function to convert text node to html

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -10,7 +11,7 @@ class TextType(Enum):
     CODE = "code"
 
 
-class TextNode():
+class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
@@ -22,7 +23,24 @@ class TextNode():
             and self.text_type == other.text_type
             and self.url == other.url
         )
+
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
-    
+
+
+def text_node_to_html_node(text_node):
+    match (text_node.text_type):
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Invalid type")
